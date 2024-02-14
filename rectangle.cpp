929d2be0d7272459c96b0ace0d12e6f9649bb916
Михаил <139983151+Gumirov-Mikhail@users.noreply.github.com>
2024-02-14 05:38:19 +0300

    Add files via upload

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,32 +1,15 @@
-#include <iostream>
-
-class rectangle {
-public:
-    rectangle () = default;
-    rectangle (double dlina, double shirina) {
-        dl = dlina;
-        sh = shirina;
-    }
-    double Perimetr () {
-        return 2 * (dl + sh);
-    }
-    double Ploshad () {
-        return dl * sh;
-    }
-    void VyvodDlinaandShirina () {
-        std::cout << dl << '\t' << sh << std::endl;
-    }
-private:
-    double dl = 0.0;
-    double sh = 0.0;
-};
-
-int main() {
-    double dlina;
-    double shirina;
-    std::cin >> dlina >> shirina;
-    rectangle Prymoygolnik1(dlina, shirina);
-    std::cout << Prymoygolnik1.Perimetr() << std::endl;
-    std::cout << Prymoygolnik1.Ploshad() << std::endl;
-    Prymoygolnik1.VyvodDlinaandShirina();
+#include "rectangle.hpp"
+rectangle::rectangle (double dlina, double shirina) {
+    dl = dlina;
+    sh = shirina;
 }
+double rectangle::Perimetr () {
+    return 2 * (dl + sh);
+}
+double rectangle::Ploshad () {
+    return dl * sh;
+}
+void rectangle::VyvodDlinaAndShirina () {
+    std::cout << "Dlina = " << dl << '\t' << "Shirina = " << sh << std::endl;
+}
+
