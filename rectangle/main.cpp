37609be82a8eacc1cad1c688b0851d3c9aa2a97d
Gumirov-Mikhail <m2309426@edu.misis.ru>
2024-02-29 23:53:02 +0300

    main

--- rectangle/main.cpp
+++ rectangle/main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
-#include "rectangle.cpp"
 
 int main() {
-    double dlina;
-    double shirina;
-    std::cin >> dlina >> shirina;
-    rectangle Prymoygolnik1(dlina, shirina);
-    std::cout << "Perimetr = " << Prymoygolnik1.Perimetr() << std::endl;
-    std::cout << "Ploshad = " << Prymoygolnik1.Ploshad() << std::endl;
-    Prymoygolnik1.VyvodDlinaAndShirina();
+    std::cout << "5";
 }
